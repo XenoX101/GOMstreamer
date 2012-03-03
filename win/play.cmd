9ea10154a13cbcda3a2b19a3369173c139e19cc2
XenoX101 <vortex01@gmail.com>
2012-03-03 19:23:12 +1100

    Update win/play.cmd

--- win/play.cmd
+++ win/play.cmd
@@ -1,5 +1,5 @@
-@set EMAIL="youremail@example.com"
-@set PASSWORD="PASSWORD"
+@set EMAIL="the_vortex01@hotmail.com"
+@set PASSWORD="gdpfre87"
 @set QUALITY="SQTest"
 @set STREAM="both"
 python ..\gomstreamer.py -e %EMAIL% -p %PASSWORD% -q %QUALITY% -s %STREAM% %*
